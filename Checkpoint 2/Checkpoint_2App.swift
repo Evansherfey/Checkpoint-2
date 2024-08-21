b8bc0763bf1172f7b8be420abed0656fef40c175
Evan <evan.sherfey@stu.barren.kyschools.us>
2024-08-21 08:50:15 -0500

    Initial Commit

---
+++ Checkpoint 2/Checkpoint_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Checkpoint_2App.swift
+//  Checkpoint 2
+//
+//  Created by Student on 8/21/24.
+//
+
+import SwiftUI
+
+@main
+struct Checkpoint_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Checkpoint 2UITests/Checkpoint_2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Checkpoint_2UITestsLaunchTests.swift
+//  Checkpoint 2UITests
+//
+//  Created by Student on 8/21/24.
+//
+
+import XCTest
+
+final class Checkpoint_2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
